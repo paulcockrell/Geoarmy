470182013a746a594a3db7a32fc55cbd0a17f190
"Paul Cockrell ext:(%22) <paulcockrell@gmail.com>
2011-03-26 15:56:54 +0000

    * Just stopped the show geocache page crashing when you clicked on the geocache., resolution was to pass the expected geocache ID   to the array from which geocashes are rendered * Added favicon, not sure it works, will have to see.

--- app/helpers/geocaches_helper.rb
+++ app/helpers/geocaches_helper.rb
@@ -9,7 +9,7 @@ module GeocachesHelper
         markers += "var geocaches = ["
         arr_length = points.count
         points.each_with_index do |p,i|
-            markers += "['#{i}',#{p[0]['lat']},#{p[0]['lon']},#{p[0]['index']},'#{p[0]['name']}']"
+            markers += "['#{p[0]['id']}',#{p[0]['lat']},#{p[0]['lon']},#{p[0]['index']},'#{p[0]['name']}']"
             if i < arr_length-1
                 markers += ","
             end
